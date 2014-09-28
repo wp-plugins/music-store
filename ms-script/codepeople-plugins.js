909cb33f3a2220b33879315544562a8f005d9c09
codepeople <codepeople@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-28 22:47:00 +0000

    The current update set a timeout if the IPN has not been called when the download page is visited.
    git-svn-id: https://plugins.svn.wordpress.org/music-store/tags/1.0.1@998173 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ms-script/codepeople-plugins.js
+++ ms-script/codepeople-plugins.js
@@ -11,5 +11,25 @@ jQuery(function($){
 		iPadUseNativeControls: false,
 		iPhoneUseNativeControls: false
 	});
-});
+    
+    timeout_counter = 10;
+    window['music_store_counting'] = function()
+    {
+        var loc = document.location.href;
+        document.getElementById( "music_store_error_mssg" ).innerHTML = timeout_text+' '+timeout_counter;
+        if( timeout_counter == 0 )
+        {
+            document.location = loc+( ( loc.indexOf( '?' ) == -1 ) ? '?' : '&' )+'timeout=1';    
+        }
+        else
+        {
+            timeout_counter--;
+            setTimeout( music_store_counting, 1000 );
+        }    
+    };
 
+    if( $( '[id="music_store_error_mssg"]' ).length ) 
+    {
+        music_store_counting();
+    }    
+});
