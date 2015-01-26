f233f2de6dfdf25660264e7b94bac8b125265908
codepeople <codepeople@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-26 20:12:53 +0000

    The current update allows install the plugin in a multisite WordPress.
    git-svn-id: https://plugins.svn.wordpress.org/music-store/tags/1.0.1@1076224 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- music-store.php
+++ music-store.php
@@ -358,21 +358,35 @@ if(!function_exists('ms_get_site_url')){
 				if ($networkwide) {
 					$old_blog = $wpdb->blogid;
 					// Get all blog ids
-					$blogids = $wpdb->get_col($wpdb->prepare("SELECT blog_id FROM $wpdb->blogs"));
+					$blogids = $wpdb->get_col( "SELECT blog_id FROM $wpdb->blogs" );
 					foreach ($blogids as $blog_id) {
 						switch_to_blog($blog_id);
-						$this->_create_db_structure();
+						$this->_create_db_structure( true );
+						update_option('ms_social_buttons', true);
 					}
 					switch_to_blog($old_blog);
 					return;
 				}
 			}
-			$this->_create_db_structure();
-            
-            // Plugin options
+			$this->_create_db_structure( true );
             update_option('ms_social_buttons', true);
             
 		}  // End register
+		
+		/* 
+		* A new blog has been created in a multisite WordPress
+		*/
+		function install_new_blog( $blog_id, $user_id, $domain, $path, $site_id, $meta ){
+			global $wpdb;
+			if ( is_plugin_active_for_network() ) 
+			{
+				$current_blog = $wpdb->blogid;
+				switch_to_blog( $blog_id );
+				$this->_create_db_structure( true );
+				update_option('ms_social_buttons', true);
+				switch_to_blog( $current_blog );
+			}
+		}
 		
 		/*
 		* Create the Music Store tables
@@ -380,11 +394,11 @@ if(!function_exists('ms_get_site_url')){
 		* @access private
 		* @return void
 		*/
-		private function _create_db_structure(){
+		private function _create_db_structure( $installing = false ){
             try{
                 global $wpdb;
                 
-                if( !empty( $_SESSION[ 'msdb_created_db' ] ) )
+                if( !$installing && !empty( $_SESSION[ 'msdb_created_db' ] ) )
                 {
                     return;
                 }	
@@ -1876,7 +1890,8 @@ if(!function_exists('ms_get_site_url')){
 	@session_start();
 	$GLOBALS['music_store'] = new MusicStore;
 	
-	register_activation_hook(__FILE__, array(&$GLOBALS['music_store'], 'register'));
+	register_activation_hook( __FILE__, array( &$GLOBALS[ 'music_store' ], 'register' ) );
+	add_action( 'wpmu_new_blog', array( &$GLOBALS[ 'music_store' ], 'install_new_blog' ), 10, 6 );
 	
 } // Class exists check
 
