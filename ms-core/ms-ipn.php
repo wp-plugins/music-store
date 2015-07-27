d1adbba733a15bb42cd5158770d787f027403df5
codepeople <codepeople@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-27 12:56:00 +0000

    The current update:
    * Corrects an issue with the use of the predefined layouts in stores protected with Secure Socket Layer (SSL).
    
    git-svn-id: https://plugins.svn.wordpress.org/music-store/tags/1.0.24@1207480 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ms-core/ms-ipn.php
@@ -0,0 +1,110 @@
+<?php
+	if( !defined( 'MS_H_URL' ) ) { echo 'Direct access not allowed.';  exit; }	
+	error_reporting( E_ERROR | E_PARSE );
+    echo 'Start IPN';
+	
+    function music_store_envelope_from( $phpmailer )
+    {
+        $ms_notification_from_email = get_option('ms_notification_from_email', MS_NOTIFICATION_FROM_EMAIL);
+        $phpmailer->Sender = $ms_notification_from_email;
+        $phpmailer->From = $ms_notification_from_email; 
+    }
+    
+    $item_name = $_POST['item_name'];
+	$item_number = $_POST['item_number'];
+	$payment_status = $_POST['payment_status'];
+	$payment_amount = $_POST['mc_gross'];
+	$payment_currency = $_POST['mc_currency'];
+	$txn_id = $_POST['txn_id'];
+	$receiver_email = $_POST['receiver_email'];
+	$payer_email = $_POST['payer_email'];
+	$payment_type = $_POST['payment_type'];
+
+	if ($payment_status != 'Completed' && $payment_type != 'echeck') exit;
+	if ($payment_type == 'echeck' && $payment_status == 'Completed') exit;
+	
+	$price = -1;
+	
+	if(!isset($_GET['id']) || !isset($_GET['purchase_id'])) exit;
+	
+	$id = $_GET['id'];
+	$_post = get_post($id);
+	if(is_null($_post)) exit;
+	switch ($_post->post_type){
+		case "ms_song":
+			$obj = new MSSong($id);
+		break;
+		case "ms_collection":
+			$obj = new MSCollection($id);
+		break;
+		default:
+			exit;
+		break;
+	}
+	
+	if (!isset($obj->price) || abs($payment_amount - $obj->price) > 0.2) exit;
+	
+	$str = "";
+	foreach ($_POST as $item => $value) $str .= $item."=".$value."\r\n";
+	
+	// Insert purchase in database
+	if($wpdb->insert(
+						$wpdb->prefix.MSDB_PURCHASE,
+						array(
+							'product_id'  => $_GET['id'],
+							'purchase_id' => $_GET['purchase_id'],
+							'date'		  => date( 'Y-m-d H:i:s'),
+							'email'		  => $payer_email,
+							'amount'	  => $payment_amount,
+							'paypal_data' => $str
+						),
+						array('%d', '%s', '%s', '%s', '%f', '%s')
+					))
+	{
+		// Increase sales in elements
+		$obj->purchases++;
+	}
+	
+	
+	
+	$ms_notification_from_email 		= get_option('ms_notification_from_email', MS_NOTIFICATION_FROM_EMAIL);
+	$ms_notification_to_email   		= get_option('ms_notification_to_email', MS_NOTIFICATION_TO_EMAIL);
+	
+	$ms_notification_to_payer_subject   = get_option('ms_notification_to_payer_subject', MS_NOTIFICATION_TO_PAYER_SUBJECT);
+	$ms_notification_to_payer_message   = get_option('ms_notification_to_payer_message', MS_NOTIFICATION_TO_PAYER_MESSAGE);
+	
+	$ms_notification_to_seller_subject  = get_option('ms_notification_to_seller_subject', MS_NOTIFICATION_TO_SELLER_SUBJECT);
+	$ms_notification_to_seller_message  = get_option('ms_notification_to_seller_message', MS_NOTIFICATION_TO_SELLER_MESSAGE);
+    
+    $dlurl = $GLOBALS['music_store']->_ms_create_pages( 'ms-download-page', 'Download Page' ); // for download-page
+    $dlurl .= ( ( strpos( $dlurl, '?' ) === false ) ? '?' : '&' );
+    
+	$information_payer = "Product: {$item_name}\n".
+						 "Amount: {$payment_amount} {$payment_currency}\n".
+						 "Download Link: ".$dlurl."ms-action=download&purchase_id={$_GET['purchase_id']}\n";
+						 
+	$information_seller = "Product: {$item_name}\n".
+						  "Amount: {$payment_amount} {$payment_currency}\n".
+						  "Buyer Email: {$payer_email}\n".
+						  "Download Link: ".$dlurl."ms-action=download&purchase_id={$_GET['purchase_id']}\n";
+						 
+	$ms_notification_to_payer_message  = str_replace("%INFORMATION%", $information_payer, $ms_notification_to_payer_message);
+	$ms_notification_to_seller_message = str_replace("%INFORMATION%", $information_seller, $ms_notification_to_seller_message);
+	
+    add_filter('phpmailer_init','music_store_envelope_from');
+    
+	// Send email to payer
+	wp_mail($payer_email, $ms_notification_to_payer_subject, $ms_notification_to_payer_message,
+            "From: \"$ms_notification_from_email\" <$ms_notification_from_email>\r\n".
+            "Content-Type: text/plain; charset=utf-8\n".
+            "X-Mailer: PHP/" . phpversion());
+
+    // Send email to seller
+	wp_mail($ms_notification_to_email , $ms_notification_to_seller_subject, $ms_notification_to_seller_message,
+			"From: \"$ms_notification_from_email\" <$ms_notification_from_email>\r\n".
+			"Content-Type: text/plain; charset=utf-8\n".
+			"X-Mailer: PHP/" . phpversion());
+
+   echo 'OK';
+   exit();
+?>
